a1f7b133ece5e1cc35b6c0fd8ca473ff1c76d47e
talygos <talison.macedoo@gmail.com>
2018-05-17 22:49:37 -0300

    First commit

---
+++ src/functions/imports.php
@@ -0,0 +1,7 @@
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="<?php echo $host ?>blog/assets/css/materialize.min.css"  media="screen,projection">
+
+<!--Custom CSS-->
+<link type="text/css" rel="stylesheet" href="<?php echo $host ?>blog/assets/css/estilo.css">
+
+<script type="text/javascript" src="<?php echo $host ?>blog/assets/js/jquery-2.1.1.min.js"></script>
